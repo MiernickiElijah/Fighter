5e7dd21af0f8b68d0a6048ccc6d205c3a5480f9f
Elijah Miernicki <miernickielijah@gmail.com>
2021-07-17 15:11:59 -0500

    updated the naming conventions for functions to work

--- models/index.js
+++ models/index.js
@@ -1,13 +1,32 @@
 const User = require('./User');
-const Project = require('./Project');
+const Comment = require('./Comment');
+const Post = require('./Post');
 
-User.hasMany(Project, {
+User.hasMany(Comment, {
   foreignKey: 'user_id',
   onDelete: 'CASCADE'
 });
 
-Project.belongsTo(User, {
+Comment.belongsTo(User, {
   foreignKey: 'user_id'
 });
 
-module.exports = { User, Project };
+Comment.belongsTo(Post , {
+  foreignKey: 'post_id'
+})
+
+Post.belongsTo(User, {
+  foreignKey: 'user_id'
+})
+
+Post.hasMany( Comment, {
+  foreignKey: 'post_id',
+  onDelete: 'CASCADE'
+});
+
+User.hasMany(Post, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
+
+module.exports = { User, Comment, Post };
